3dc8035c3322f7248f59826c86e2724d1cf5f4da
aslamismail <144669541+aslamismail@users.noreply.github.com>
2024-04-16 16:27:39 +0530

    Add files via upload

---
+++ C4/Q5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Q5 {
+public static void main(String[] args){
+System.out.println("Aslam Ismail\n23MCA022\n15-04-24");
+LinkedList<String> L=new LinkedList<>();
+L.add("Gold");
+L.add("Silver");
+L.add("Bronze");
+L.add(0,"Olympics Medals");
+System.out.println(L);
+L.remove("Bronze");
+System.out.println(L);
+L.remove(2);
+System.out.println(L);
+L.removeLast();
+System.out.println(L);
+L.removeFirst();
+System.out.println(L);
+
+}
+}
